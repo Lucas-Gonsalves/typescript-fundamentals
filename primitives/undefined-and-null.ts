36950d011608d1f983cc57b00fa21766315fd516
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-05-02 11:09:12 -0300

    feat(primitives): create undefined and null

---
+++ primitives/undefined-and-null.ts
@@ -0,0 +1,18 @@
+// undefined
+let value: number
+value = 12
+console.log(value)
+
+let user = {
+  name: 'Lucas'
+}
+// console.log(user.email)  ...undefined
+
+
+// null
+let email = null
+console.log(email)
+
+if (!email) {
+  console.log('Insert the email')
+}
