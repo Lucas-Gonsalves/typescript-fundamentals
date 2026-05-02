3e87643231e4b34111935fbe65021371f48b4ab5
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-05-02 10:30:12 -0300

    feat(primitive): create any and array

---
+++ primitives/any.ts
@@ -0,0 +1,6 @@
+let comment  // any
+
+// with type any the variable acepts any value
+comment = "Test"
+comment = 45
+comment = true
---
+++ primitives/array.ts
@@ -0,0 +1,6 @@
+// explicit typing, type annotation
+let names: string[] = ["Lucas", "Anna"]
+let numbers: number[] = [1, 2, 3]
+
+// enference typing
+let pet = ["Dog", "cat"]
